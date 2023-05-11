219e08f26f7b2034c42042a8e01e26337cbe967b
Adam Nagle <nagle.adam@gmail.com>
2023-05-11 09:58:28 -0500

    circle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Stadium1Component } from './stadium1/stadium1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/stadium1', pathMatch: 'full'},
+  { path: 'stadium1', component: Stadium1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Stadium1Component } from './stadium1/stadium1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Stadium1Component
   ],
   imports: [
     BrowserModule,
